ec12372a4f4fb3d7ea9d75f6a94a49ef8c3c5e6f
Landon Turner <lturner@teamworks.com>
2020-06-05 19:06:29 -0400

    add paymentMethod create

--- crudOperations/paymentOperations/paymentRouter.js
+++ crudOperations/paymentOperations/paymentRouter.js
@@ -9,29 +9,30 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST_KEY); //Change S
 router.post('/', async (req, res) => {
   
     const data = req.body;
-    console.log('data to payment intent', data)
+    // console.log('data to payment intent', data)
     const amount = data.amount;
     const { domain_name } = data.token
     const { orderToken } = data.token // this will need to be the order token to send the order
     let application_fee;
-    let confirmation;
-    let paymentMethod = {
-      type: data.token.card.object,
-      billing_details: {
-        address: {
-          city: data.token.card.address_city,
-          country: data.token.card.address_country,
-          line1: data.token.card.address_line1,
-          line2: data.token.card.address_line2,
-          postal_code: data.token.card.address_zip,
-          state: data.token.card.address_state
-        },
-        email: data.email,
-        name: data.token.card.name,
-        phone: null // set for null unless it is passed later
-      },
-      metadata: {}
-    };
+    let confirmation;// to be used for client secret in confirm payment
+    // let paymentMethod = {
+    //   type: data.token.card.object,
+    //   billing_details: {
+    //     address: {
+    //       city: data.token.card.address_city,
+    //       country: data.token.card.address_country,
+    //       line1: data.token.card.address_line1,
+    //       line2: data.token.card.address_line2,
+    //       postal_code: data.token.card.address_zip,
+    //       state: data.token.card.address_state
+    //     },
+    //     email: data.email,
+    //     name: data.token.card.name,
+    //     phone: null // set for null unless it is passed later
+    //   },
+    //   metadata: {}
+    // };
+    let method;
     const calculateOrder = (items) => {
       // Determine application fee here
       // passing array of expenses
@@ -107,13 +108,31 @@ router.post('/', async (req, res) => {
                 error,
                 message: "Unable to add this order, its not you.. its me"
               });
-            }
+            };
+
+            // payment method for payment intent
+            await stripe.paymentMethods.create({
+              type: data.type,
+              card: {token: data.token.id}
+            })
+            .then(function(paymentMethod){
+              try {
+                return method = paymentMethod;
+              } catch (error) {
+                console.log('PAYMENT METHOD ERROR', error);
+                return res.status(500).send({
+                  error: err.message
+                })
+              }
+            });
 
             await stripe.paymentIntents.create({
                 payment_method_types: ['card'],
                 amount: amount,
                 currency: 'usd', // currency is passed to obj on feature/buyer-address branch
-                application_fee_amount: application_fee * 100, // fee will be what scalable press needs to print given product and come to us
+                application_fee_amount: application_fee * 100,
+                // fee will be what scalable press needs to print given product and come to us
+                confirm: true 
               }, {
                   stripeAccount: acctStripe
               }).then(function(paymentIntent) {
@@ -131,20 +150,6 @@ router.post('/', async (req, res) => {
                 }
               });
 
-              // confirm payment with payment intent
-              await stripe.confirmCardPayment(confirmation, {
-                payment_method: paymentMethod
-              })
-              .then(function(result){
-                try {
-                  return res.status(201).json(result.paymentIntent);
-                } catch (error) {
-                  console.log('CONFIRM CARD PAYMENT ERROR', error);
-                  return res.status(500).send({
-                    error: err.message
-                  })
-                }
-              }) 
         })
     });
 });
